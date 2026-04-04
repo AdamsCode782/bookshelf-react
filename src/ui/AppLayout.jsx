8551d89eeb96b466f51a1fc83a620c8ce99445f3
Adam Steele <asteeledesign@gmail.com>
2026-04-04 15:45:21 -0400

    fixed heading blur

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,11 +9,12 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {isLoading && <Loader />}
+      
 
       <Header />
 
-      <div className="overflow-scroll">
+      <div className="relative overflow-scroll">
+        {isLoading && <Loader />}
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
